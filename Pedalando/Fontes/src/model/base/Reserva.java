cbe564b61dde5139293e0d5eb3be7fb8c994d651
daniel.fsalves@gmail.com <daniel.fsalves@gmail.com@7a354412-9265-d797-a54e-947241ed519f>
2010-11-17 18:48:07 +0000

    Novas classes adicionades e bases preenchidas. Acho que precisaremos de devoluções.

--- Pedalando/Fontes/src/model/base/Reserva.java
+++ Pedalando/Fontes/src/model/base/Reserva.java
@@ -1,5 +1,41 @@
 package model.base;
 
-public class Reserva {
+import java.util.Date;
 
+public class Reserva {
+	private long id;
+	private Estacao estacao;
+	private Usuario usuario;
+	private Estacao destino;
+	private Date data;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Estacao getEstacao() {
+		return estacao;
+	}
+	public void setEstacao(Estacao estacao) {
+		this.estacao = estacao;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Estacao getDestino() {
+		return destino;
+	}
+	public void setDestino(Estacao destino) {
+		this.destino = destino;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
 }
