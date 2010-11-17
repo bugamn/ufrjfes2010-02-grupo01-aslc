1fdf98bf7d84a28fe44708c04612663a4cee99f1
gustavorl.89@gmail.com <gustavorl.89@gmail.com@7a354412-9265-d797-a54e-947241ed519f>
2010-11-17 14:06:17 +0000

    Início do código, base do modelo criada.

---
+++ Pedalando/Fontes/src/model/base/Bicicleta.java
@@ -0,0 +1,38 @@
+package model.base;
+
+import javax.persistence.Id;
+
+public class Bicicleta {
+	
+	@Id
+	private String placa;
+	
+	private String tipo;
+	
+	private Estacao estacao;
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Estacao getEstacao() {
+		return estacao;
+	}
+
+	public void setEstacao(Estacao estacao) {
+		this.estacao = estacao;
+	}
+	
+}
