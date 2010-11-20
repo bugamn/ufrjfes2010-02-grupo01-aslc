78e2be78acb044ce21fba76b15493c6fd201f278
daniel.fsalves <daniel.fsalves@7a354412-9265-d797-a54e-947241ed519f>
2010-11-20 23:51:54 +0000

    Trabalhando.

--- trunk/Pedalando/Fontes/src/controller/BicicletasController.java
+++ trunk/Pedalando/Fontes/src/controller/BicicletasController.java
@@ -5,21 +5,34 @@ import java.util.List;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import model.base.Bicicleta;
+import model.base.Estacao;
 import model.dao.BicicletaDAO;
+import model.dao.EstacaoDAO;
 
 @Resource
 public class BicicletasController {
 	private final BicicletaDAO bicicletaDAO;
+	private final EstacaoDAO estacaoDAO;
 	private final Result result;
 
-	public BicicletasController(BicicletaDAO bicicletaDAO, Result result) {
+	public BicicletasController(BicicletaDAO bicicletaDAO, EstacaoDAO estacaoDAO, Result result) {
 		this.bicicletaDAO = bicicletaDAO;
+		this.estacaoDAO = estacaoDAO;
 		this.result = result;
 	}
 	
-	public void adiciona(Bicicleta bicicleta) {
+	public void adiciona(Bicicleta bicicleta, int id) {
+		bicicleta.setEstacao(estacaoDAO.encontra(id));
 		bicicletaDAO.salva(bicicleta);
 		result.redirectTo(this).lista(".*", ".*", ".*");
+	}
+	
+	public List<Estacao> formulario() {
+		return estacaoDAO.lista(".*");
+	}
+	
+	public List<Estacao> busca() {
+		return estacaoDAO.lista(".*");
 	}
 
 	public List<Bicicleta> lista(String placaBusca, String tipoBusca, String estacaoBusca) {
@@ -34,7 +47,6 @@ public class BicicletasController {
 		}
 		placaBusca = ".*" + placaBusca + ".*";
 		tipoBusca = ".*" + estacaoBusca + ".*";
-		estacaoBusca = ".*" + estacaoBusca + ".*";
 		return bicicletaDAO.lista(placaBusca, tipoBusca, estacaoBusca);		
 	}
 }
--- trunk/Pedalando/Fontes/src/controller/EstacoesController.java
+++ trunk/Pedalando/Fontes/src/controller/EstacoesController.java
@@ -4,16 +4,54 @@ import java.util.List;
 
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import model.base.Bicicleta;
 import model.base.Estacao;
+import model.dao.BicicletaDAO;
 import model.dao.EstacaoDAO;
 
 @Resource
 public class EstacoesController {
 	private final EstacaoDAO estacaoDAO;
+	private final BicicletaDAO bicicletaDAO;
 	private final Result result;
 	
-	public EstacoesController(EstacaoDAO estacaoDAO, Result result) {
+	public class Consulta {
+		private Estacao estacao;
+		private List<Dados> lista;
+		public Estacao getEstacao() {
+			return estacao;
+		}
+		public void setEstacao(Estacao estacao) {
+			this.estacao = estacao;
+		}
+		public List<Dados> getLista() {
+			return lista;
+		}
+		public void setLista(List<Dados> lista) {
+			this.lista = lista;
+		}
+	}
+	
+	public class Dados {
+		private String tipo;
+		private int quantidade;
+		public String getTipo() {
+			return tipo;
+		}
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+		public int getQuantidade() {
+			return quantidade;
+		}
+		public void setQuantidade(int quantidade) {
+			this.quantidade = quantidade;
+		}
+	}
+	
+	public EstacoesController(EstacaoDAO estacaoDAO, BicicletaDAO bicicletaDAO, Result result) {
 		this.estacaoDAO = estacaoDAO;
+		this.bicicletaDAO = bicicletaDAO;
 		this.result = result;
 	}
 	
@@ -33,5 +71,19 @@ public class EstacoesController {
 		}
 		nomeBusca = ".*" + nomeBusca + ".*";
 		return estacaoDAO.lista(nomeBusca);
+	}
+	
+	//Em desenvolvimento deverá retornar uma lista de Consulta
+	public void consulta(String nomeBusca) {
+		List<Estacao> lista;
+		if (nomeBusca == null) {
+			nomeBusca = "";
+		}
+		nomeBusca = ".*" + nomeBusca + ".*";
+		lista = estacaoDAO.lista(nomeBusca);
+		
+		for (Estacao estacao : lista) {
+			
+		}
 	}
 }
