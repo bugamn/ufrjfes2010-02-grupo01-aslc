eee11e71b188c9d7b06041f09bc3af7df9d38bdb
daniel.fsalves <daniel.fsalves@7a354412-9265-d797-a54e-947241ed519f>
2010-11-21 16:38:07 +0000

    Testes

--- trunk/Pedalando/Fontes/src/teste/controller/EstacoesControllerTest.java
+++ trunk/Pedalando/Fontes/src/teste/controller/EstacoesControllerTest.java
@@ -2,38 +2,188 @@ package teste.controller;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
+import model.base.Bicicleta;
+import model.base.Estacao;
+import model.dao.BicicletaDAO;
+import model.dao.EstacaoDAO;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.View;
+
+import controller.EstacoesController;
+
 public class EstacoesControllerTest {
+	private static EstacoesController estacoesController;
+	private static String nomeEstacao = "CCMN";
+	private static String outraEstacao = "CCS";
+	private static String placaBicicleta = "XKCD";
+	private static String tipoBicicleta = "normal";
+	private static int capacidadeEstacao = 100;
+	private static EstacaoDAO estacaoDAO = new EstacaoDAO();
+	private static BicicletaDAO bicicletaDAO = new BicicletaDAO();
+	private static Estacao estacao = new Estacao();
+	private static Estacao estacao2 = new Estacao();
+	private static Bicicleta bicicleta = new Bicicleta();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Test
-	public void testEstacoesController() {
-		fail("Not yet implemented");
+		
+		estacao.setCapacidade(capacidadeEstacao );
+		estacao.setNome(nomeEstacao);
+		
+		estacaoDAO.salva(estacao);
+		
+		estacao2.setCapacidade(capacidadeEstacao);
+		estacao2.setNome(outraEstacao);
+		
+		estacaoDAO.salva(estacao2);
+		
+		bicicleta.setEstacao(estacao);
+		bicicleta.setPlaca(placaBicicleta);
+		bicicleta.setTipo(tipoBicicleta);
+		
+		estacoesController = new EstacoesController(estacaoDAO, bicicletaDAO, new Result() {
+			
+			@Override
+			public boolean used() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public <T extends View> T use(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T redirectTo(T arg0) {
+				// TODO Auto-generated method stub
+				return arg0;
+			}
+			
+			@Override
+			public <T> T redirectTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void redirectTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public <T> T permanentlyRedirectTo(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T permanentlyRedirectTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void permanentlyRedirectTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Result on(Class<? extends Exception> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T of(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T of(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void nothing() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void notFound() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Map<String, Object> included() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Result include(String arg0, Object arg1) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T forwardTo(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T forwardTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void forwardTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Test
 	public void testAdiciona() {
-		fail("Not yet implemented");
-	}
+		Estacao novaEstacao = new Estacao();
+		String novoNome = "CT";
+		int novaCapacidade = 200;
+		int tamanho = estacaoDAO.lista(".*").size();
+		
+		novaEstacao.setNome(novoNome);
+		novaEstacao.setCapacidade(novaCapacidade);
 
-	@Test
-	public void testFormulario() {
-		fail("Not yet implemented");
-	}
+		estacoesController.adiciona(novaEstacao);
 
-	@Test
-	public void testBusca() {
-		fail("Not yet implemented");
+		assertEquals("O método adiciona não funciona.", tamanho + 1, estacaoDAO.lista(".*").size());
+		
+		estacaoDAO.remove(novaEstacao);
 	}
 
 	@Test
 	public void testLista() {
-		fail("Not yet implemented");
+		assertEquals("O método lista não funciona de modo genérico.", 
+				estacaoDAO.lista(".*").size(), estacoesController.lista(".*").size());
+		assertEquals("O método lista não funciona quando encontra uma só.", 
+				estacaoDAO.lista(nomeEstacao).size(), estacoesController.lista(nomeEstacao).size());
+		assertEquals("O método lista não funciona quando não encontra.", 
+				estacaoDAO.lista(nomeEstacao + nomeEstacao).size(), estacoesController.lista(nomeEstacao + nomeEstacao).size());
 	}
 
 	@Test
--- trunk/Pedalando/Fontes/src/teste/controller/UsuariosControllerTest.java
+++ trunk/Pedalando/Fontes/src/teste/controller/UsuariosControllerTest.java
@@ -2,33 +2,174 @@ package teste.controller;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
+import model.base.Usuario;
+import model.base.Usuario.Permissao;
+import model.dao.UsuarioDAO;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.View;
+
+import controller.UsuariosController;
+
 public class UsuariosControllerTest {
+	private static UsuariosController usuariosController;
+	private static UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private static Usuario usuario = new Usuario();
+	private static Usuario usuario2 = new Usuario();
+	private static String nome = "Zé Sá";
+	private static String nome2 = "Mário";
+	private static String cpf = "12345678900";
+	private static String cpf2 = "98765432100";
+	private static String endereco = "Imaginação";
+	private static String endereco2 = "Armário";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Test
-	public void testUsuariosController() {
-		fail("Not yet implemented");
+		usuario.setCpf(cpf);
+		usuario.setEndereco(endereco);
+		usuario.setNome(nome);
+		usuario.setPermissao(Permissao.ADMIN);
+		usuarioDAO.salva(usuario);
+		
+		usuario2.setCpf(cpf2);
+		usuario2.setEndereco(endereco2);
+		usuario2.setNome(nome2);
+		usuario2.setPermissao(Permissao.OPERATOR);
+		usuarioDAO.salva(usuario2);
+		
+		usuariosController = new UsuariosController(usuarioDAO, new Result() {
+			
+			@Override
+			public boolean used() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public <T extends View> T use(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T redirectTo(T arg0) {
+				// TODO Auto-generated method stub
+				return arg0;
+			}
+			
+			@Override
+			public <T> T redirectTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void redirectTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public <T> T permanentlyRedirectTo(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T permanentlyRedirectTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void permanentlyRedirectTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Result on(Class<? extends Exception> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T of(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T of(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void nothing() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void notFound() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Map<String, Object> included() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Result include(String arg0, Object arg1) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T forwardTo(T arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public <T> T forwardTo(Class<T> arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void forwardTo(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Test
 	public void testAdiciona() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFormulario() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBusca() {
-		fail("Not yet implemented");
+		Usuario novo = new Usuario();
+		String novoNome = "Euzim";
+		String novoCpf = "65432178900";
+		String novoEnd = "Algum lugar";
+		
+		int tamanho = usuarioDAO.lista(".*", ".*", ".*").size();
+		
+		novo.setCpf(novoCpf);
+		novo.setEndereco(novoEnd);
+		novo.setNome(novoNome);
+		
+		usuariosController.adiciona(novo, 3);
+		
+		assertEquals("O método adiciona não funciona.", tamanho + 1, usuarioDAO.lista(".*", ".*", ".*").size());
 	}
 
 	@Test
