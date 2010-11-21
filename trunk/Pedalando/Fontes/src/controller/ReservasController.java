3b3a4ce79c34d2288d0dcb4c400df081ba696d98
daniel.fsalves <daniel.fsalves@7a354412-9265-d797-a54e-947241ed519f>
2010-11-21 02:36:48 +0000

    Mudanças.

--- trunk/Pedalando/Fontes/src/controller/ReservasController.java
+++ trunk/Pedalando/Fontes/src/controller/ReservasController.java
@@ -1,37 +1,55 @@
 package controller;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import model.base.Reserva;
+import model.dao.BicicletaDAO;
+import model.dao.EstacaoDAO;
 import model.dao.ReservaDAO;
+import model.dao.UsuarioDAO;
 
 @Resource
 public class ReservasController {
 	private final ReservaDAO reservaDAO;
+	private final EstacaoDAO estacaoDAO;
+	private final UsuarioDAO usuarioDAO;
 	private final Result result;
 	
-	public ReservasController(ReservaDAO reservaDAO, Result result) {
+	public ReservasController(ReservaDAO reservaDAO, EstacaoDAO estacaoDAO, UsuarioDAO usuarioDAO, Result result) {
 		this.reservaDAO = reservaDAO;
+		this.estacaoDAO = estacaoDAO;
+		this.usuarioDAO = usuarioDAO;
 		this.result = result;
 	}
 
-	public void adiciona(Reserva reserva) {
+	public void adiciona(int origem, int destino, String cpf, int dia, int mes, int ano, int hora, int minuto) {
+		Reserva reserva = new Reserva();
+		
+		reserva.setEstacao(estacaoDAO.encontra(origem));
+		reserva.setDestino(estacaoDAO.encontra(destino));
+		reserva.setUsuario(usuarioDAO.encontra(cpf));
+		reserva.setData(new Date(new GregorianCalendar(ano, mes, dia, hora, minuto).getTimeInMillis()));		
+		
 		reservaDAO.salva(reserva);
-		result.redirectTo(this).lista(".*", ".*", ".*", null, null);
+		//result.redirectTo(this).lista(".*", ".*", ".*", null, null);
 	}
 	
 	
-	public List<Reserva> lista(String estacaoBusca, String cpfBusca, String destinoBusca, Date before, Date after) {
-		if (before == null) {
-			
-		}
-		if (after == null) {
-			
-		}
-		return reservaDAO.lista(estacaoBusca, cpfBusca, destinoBusca, before, after);
+	public List<Reserva> lista(String estacaoBusca, String cpfBusca, String destinoBusca, 
+			int diaAntes, int mesAntes, int anoAntes, int diaDepois, int mesDepois, int anoDepois) {
+		Date antes, depois;
+		antes = new Date(new GregorianCalendar(anoAntes, mesAntes, diaAntes).getTimeInMillis());
+		depois = new Date(new GregorianCalendar(anoDepois, mesDepois, diaDepois).getTimeInMillis());
+		
+		estacaoBusca = ".*" + estacaoBusca + ".*";
+		cpfBusca = ".*" + cpfBusca + ".*";
+		destinoBusca = ".*" + destinoBusca + ".*";
+		
+		return reservaDAO.lista(estacaoBusca, cpfBusca, destinoBusca, antes, depois);
 	}
 
 }
