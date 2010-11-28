ecb610b9d0dd1361289a39bba6f8e3feb9b09b59
blimacardoso@gmail.com <blimacardoso@gmail.com@7a354412-9265-d797-a54e-947241ed519f>
2010-11-28 22:47:41 +0000

    Fiz alguns ajustes nos campos e fieldset. O menu ainda esta com problema, mas é possivel navegar nele. Devo consertar em breve. Eu queria fazer o header com flash, mas ñ existe distribuição do adobe para linux e eu ñ sabia :P Se vocês tiverem alguma sugestão ou não gastaram de algo me avisem.

--- trunk/Pedalando/Fontes/WebContent/js/menu_dropdown.js
+++ trunk/Pedalando/Fontes/WebContent/js/menu_dropdown.js
@@ -5,8 +5,10 @@
 $(document).ready(function() {
 	$('li.cabeca_menu ul').css('display', 'none');
 	$('li.cabeca_menu').hover(function() {
+		$('ul',this).addClass("hover");
 		$('ul', this).css('display', 'block');
 	}, function() {
+		$('ul',this).removeClass("hover");
 		$('ul', this).css('display', 'none');
 	});
 
