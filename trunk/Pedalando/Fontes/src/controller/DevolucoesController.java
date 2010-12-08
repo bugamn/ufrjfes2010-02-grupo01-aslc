01fcdb6cfb6c0b07faa8937f6df5a8c189c5a9ce
diegofpsouza@gmail.com <diegofpsouza@gmail.com@7a354412-9265-d797-a54e-947241ed519f>
2010-12-08 15:52:31 +0000

    Javadoc

--- trunk/Pedalando/Fontes/src/controller/DevolucoesController.java
+++ trunk/Pedalando/Fontes/src/controller/DevolucoesController.java
@@ -24,7 +24,20 @@ public class DevolucoesController {
 	private final DevolucaoDAO devolucaoDAO;
 	private final Result result;
 	private final Validator validator;
-	
+
+	/**
+	 * Construtor publico para o Controller de Devolucoes
+	 * @param locacaoDAO
+	 * Um objeto de acesso aos dados de Locacao
+	 * @param estacaoDAO
+	 * Um objeto de acesso aos dados de Estacao
+	 * @param devolucaoDAO
+	 * Um objeto de acesso aos dados de Devolucao
+	 * @param result
+	 * Um objeto de result do VRaptor
+	 * @param validator
+	 * Um objeto de validacao do VRaptor
+	 */
 	public DevolucoesController(LocacaoDAO locacaoDAO, EstacaoDAO estacaoDAO, DevolucaoDAO devolucaoDAO, Result result, Validator validator) {
 		
 		this.locacaoDAO = locacaoDAO;
@@ -34,6 +47,13 @@ public class DevolucoesController {
 		this.validator = validator;
 	}
 	
+	/**
+	 * Método para adicionar uma nova devolucao
+	 * @param idLocacao
+	 * Id da locacao que foi finalizada com a devolucao
+	 * @param idEstacao
+	 * Id da estação que recebeu a bicicleta
+	 */
 	public void adiciona(int idLocacao, int idEstacao) {
 		Locacao locacao = locacaoDAO.encontra(idLocacao);
 		Estacao estacao = estacaoDAO.encontra(idEstacao);
@@ -57,6 +77,13 @@ public class DevolucoesController {
 		result.redirectTo(this).lista("", "", 0, 0, 0, 0, 0, 0);
 	}
 	
+	/**
+	 * Metodo para preparar um formulario para criacao de uma nova devolucao
+	 * @param id
+	 * Id da locacao que sera finalizada com a devolucao
+	 * @return
+	 * Um objeto com estacoes e a locacao a ser terminada para fornecer a jsp os dados para completar o formulário
+	 */
 	public DevolucaoVO formulario(int id) {
 		DevolucaoVO devolucaoVO = new DevolucaoVO();
 		
@@ -70,8 +97,31 @@ public class DevolucoesController {
 		return devolucaoVO;
 	}
 	
+	/**
+	 * Metodo que o VRaptor chama para criar um formulario de busca
+	 */
 	public void busca() {}
 	
+	/**
+	 * Metodo para formacao de listas para buscas
+	 * @param estacaoBusca
+	 * Expressao regular para o nome da estacao
+	 * @param nomeBusca
+	 * Expressao regular para o nome do usuario
+	 * @param diaAntes
+	 * Dia antes, 0 para qualquer (aproxima para 1), para o limite de data
+	 * @param mesAntes
+	 * Mes antes, 0 para qualquer (aproxima para Janeiro), para o limite de data
+	 * @param anoAntes
+	 * Ano antes, 0 para qualquer (aproxima para 2010), para o limite de data
+	 * @param diaDepois
+	 * Dia depois, para o limite de data
+	 * @param mesDepois
+	 * Mes depois, para o limite de data
+	 * @param anoDepois
+	 * Ano depois, para o limite de data
+	 * @return
+	 */
 	public List<Devolucao> lista(String estacaoBusca, String nomeBusca, 
 			int diaAntes, int mesAntes, int anoAntes, int diaDepois, int mesDepois, int anoDepois) {
 		Date antes, depois;
